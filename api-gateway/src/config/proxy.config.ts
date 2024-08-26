9277a185f68d71948c3bcfbd3ca135622398bd08
PhucNguyenHoang <hoangphucnguyen112002@gmail.com>
2024-08-26 14:52:13 +0700

    change: add env

--- api-gateway/src/config/proxy.config.ts
+++ api-gateway/src/config/proxy.config.ts
@@ -1,8 +1,11 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 @Injectable()
 export class ProxyMiddleware implements NestMiddleware {
+  constructor(private readonly configService: ConfigService) {}
+
   use(req: any, res: any, next: () => void) {
     // Handle requests to the stock-data-service and websocket-service
     if (
@@ -11,13 +14,13 @@ export class ProxyMiddleware implements NestMiddleware {
         req.url.startsWith("/socket.io/?EIO=4&transport=websocket"))
     ) {
       createProxyMiddleware({
-        target: "http://websocket-service:8082",
+        target: this.configService.get("WEBSOCKET_SERVICE_URL"),
         ws: true,
         changeOrigin: true
       })(req, res, next);
     } else {
       createProxyMiddleware({
-        target: "http://stock-data-service:8081",
+        target: this.configService.get("STOCK_DATA_SERVICE_URL"),
         changeOrigin: true
       })(req, res, next);
     }
